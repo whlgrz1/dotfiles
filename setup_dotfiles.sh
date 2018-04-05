7d605eb381c1f4d8748751570de93df05efa1f05
songyuan <rocksongyuan@gmail.com>
2018-04-04 21:03:58 -0400

    add a setup file

---
+++ setup_dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#clear configuration files if already exist
+#need to put the dotfile directory just under the home directory
+if [ -f "../.bashrc" ];then
+rm -rf ~/.bashrc
+fi
+if [ ! -d "../.ssh/" ];then
+mkdir ~/.ssh
+fi
+if [ -f "../.ssh/config" ];then
+rm -rf ~/.ssh/config
+fi
+
+#set symbolic links
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/ssh-config ~/.ssh/config
